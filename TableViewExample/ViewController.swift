9a4568049a434b94c16f2a208fc87989b9ac6bec
maciekmantis <maciej.scislowski@gmail.com>
2014-06-23 07:44:10 +0200

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Maciek on 23.06.2014.
+//  Copyright (c) 2014 Maciek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
